015f5243a22f57758841298ee9d6187f23198e53
congenial-principle-5422 <christinajenkins10@gmail.com>
2020-03-18 00:49:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,4 +4,4 @@ var chocolateBars = [ "snicker", "hundred grand", "kitkat", "skittles"];
 //addElementToBeginningOfArray
 var chocolateBars = [ "snicker", "hundred grand", "kitkat", "skittles"];
 
-[...friends, "Ross"] // ["Joey", "Chandler", "Ross"]
+["mints", ...chocolateBars,];
